f5d33ec7e5224f1570ef78eef4bdcb7830ca876b
Ryan Gibb <ryangibb321@gmail.com>
2021-01-27 16:55:30 +0000

    root fix

--- scripts.js
+++ scripts.js
@@ -4,25 +4,28 @@ navbarItems = [
 		href: "index.html",
 		name: "home"
 	}
-]
+];
 
 function setNavbarActiveTab() {
 	const navbar = document.createElement('ul');
 	navbar.classList.add("navbar");
 	const pagepath = window.location.pathname;
 	const pagename = pagepath.substring(pagepath.lastIndexOf('/') + 1);
+	if (pagename == "") {
+		pagename = "index.html";
+	}
 	for (i = 0; i < navbarItems.length; i++) {
 		link = document.createElement('a');
 		link.href = navbarItems[i].href;
 		link.innerText = navbarItems[i].name;
 		if (navbarItems[i].href == pagename) {
-			link.classList.add("active")
+			link.classList.add("active");
 		}
-		listItem = document.createElement('li')
-		listItem.appendChild(link)
-		navbar.appendChild(listItem)
+		listItem = document.createElement('li');
+		listItem.appendChild(link);
+		navbar.appendChild(listItem);
 	}
-	links = navbar.getElementsByTagName("a")
+	links = navbar.getElementsByTagName("a");
 	for (i = 0; i < links.length; i++) {
 	}
 	document.body.prepend(navbar);
