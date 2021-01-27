81f680ad9615604468a82e26db923092a3b72644
Ryan Gibb <ryangibb321@gmail.com>
2021-01-27 16:52:51 +0000

    Pure JS navbar

--- scripts.js
+++ scripts.js
@@ -1,22 +1,31 @@
 
-navbarHMLT = '\
-	<ul class="navbar">\
-		<li><a href="index.html">home</a></li>\
-	</ul>\
-'
+navbarItems = [
+	{
+		href: "index.html",
+		name: "home"
+	}
+]
 
 function setNavbarActiveTab() {
-	const navbar = document.createElement('div');
-	navbar.innerHTML = navbarHMLT;
+	const navbar = document.createElement('ul');
+	navbar.classList.add("navbar");
 	const pagepath = window.location.pathname;
 	const pagename = pagepath.substring(pagepath.lastIndexOf('/') + 1);
+	for (i = 0; i < navbarItems.length; i++) {
+		link = document.createElement('a');
+		link.href = navbarItems[i].href;
+		link.innerText = navbarItems[i].name;
+		if (navbarItems[i].href == pagename) {
+			link.classList.add("active")
+		}
+		listItem = document.createElement('li')
+		listItem.appendChild(link)
+		navbar.appendChild(listItem)
+	}
 	links = navbar.getElementsByTagName("a")
 	for (i = 0; i < links.length; i++) {
-		if (links[i].attributes.href.value == pagename) {
-			links[i].classList.add("active")
-		}
 	}
-	document.getElementById("navbar-div").innerHTML = navbar.innerHTML;
+	document.body.prepend(navbar);
 }
 
 document.addEventListener("DOMContentLoaded", setNavbarActiveTab);
