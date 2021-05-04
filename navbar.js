4448a5124e93e2e1aaa14f4a37c1c37d33e9be5d
Ryan Gibb <ryangibb321@gmail.com>
2021-05-04 20:22:35 +0100

    changed navbar to use absolute paths

--- navbar.js
+++ navbar.js
@@ -1,40 +1,22 @@
 
 navbarItems = [
 	{
-		href: "index.html",
+		href: "/index.html",
 		name: "home"
 	},
 	{
-		href: "software.html",
+		href: "/software.html",
 		name: "software"
 	},
 	{
-		href: "projects/index.html",
+		href: "/projects/index.html",
 		name: "projects"
 	}
 ];
 
 function setNavbarActiveTab() {
-	// Gets depth of HTML page relative to project root
-	const depth = 
-		document.getElementsByTagName('script')[0].getAttribute('depth')
-		|| 0;
 	var pagepath = window.location.pathname;
 
-	// Gets path of page relative to project root (with depth number of /'s)
-	var index = pagepath.length;
-	var i;
-	for (i = 0; i <= depth; i++) {
-		console.log(index)
-		const new_index = pagepath.lastIndexOf('/', index - 1);
-		if (new_index == -1) {
-			break;
-		}
-		index = new_index;
-	}
-	if (index != -1) {
-		pagepath = pagepath.substring(index + 1, pagepath.length)
-	}
 	// If the path is a directory, append with "index.html"
 	if (pagepath[pagepath.length - 1] == "/") {
 		pagepath += "index.html";
@@ -46,7 +28,7 @@ function setNavbarActiveTab() {
 	navbar.id = "navbar";
 	for (i = 0; i < navbarItems.length; i++) {
 		link = document.createElement('a');
-		link.href = "../".repeat(depth) + navbarItems[i].href;
+		link.href = navbarItems[i].href;
 		link.innerText = navbarItems[i].name;
 		console.log(pagepath);
 		if (navbarItems[i].href == pagepath) {
