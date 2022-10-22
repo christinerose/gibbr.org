27e314b7329ea68a2ff5028c4dcb61ad05246dd2
Ryan Gibb <ryangibb321@gmail.com>
2022-10-22 16:56:32 +0100

    build cv from input flake rather than statically store

--- flake.nix
+++ flake.nix
@@ -1,12 +1,17 @@
 {
   description = "A flake for building https://gibbr.org";
 
-  inputs.flake-compat = {
-    url = "github:edolstra/flake-compat";
-    flake = false;
+  inputs = {
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+    cv.url = "github:RyanGibb/cv";
+    cv.inputs.nixpkgs.follows = "nixpkgs";
+    cv.inputs.flake-utils.follows = "flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, cv, ... }:
     (flake-utils.lib.eachDefaultSystem
       (system:
         let pkgs = nixpkgs.legacyPackages.${system}; in
@@ -16,21 +21,17 @@
 
             src = self;
 
-            buildInputs = [
-              pkgs.rsync
-              pkgs.pandoc
-            ];
+            buildInputs = [ pkgs.pandoc ];
 
             installPhase = ''
               mkdir -p $out
-              rsync -a --exclude '*.md' --exclude 'result' --exclude '.*' . $out
+              ${pkgs.rsync}/bin/rsync -a --exclude '*.md' --exclude 'result' --exclude '.*' . $out
+              cp ${cv.defaultPackage.${system}}/*.pdf $out/resources/cv.pdf
             '';
           };
           
           devShells.default = pkgs.mkShell {
-            buildInputs = [
-              pkgs.pandoc
-            ];
+            buildInputs = [ pkgs.pandoc ];
           };
         }
       )
